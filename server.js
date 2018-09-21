2e6651348fe60fd1c9a75d2bd6c52ede9bd6be40
Douglas Porto <douglasalexandre7@gmil.com>
2018-09-20 21:28:32 -0300

    Mutation

--- server.js
+++ server.js
@@ -1,49 +1,63 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const dev = [
-    { 
-        id: 1,
-        language: 'Javascript', 
-        typelanguage: 'Frontend',
-        testing: false
-    },
-    { 
-        id: 2,
-        language: 'PHP', 
-        typelanguage: 'Backend',
-        testing: false
-    },
-    { 
-        id: 3,
-        language: 'Graphql', 
-        typelanguage: 'Backend',
-        testing: true
-    },
+const devs = [
+  {
+    id: 1,
+    language: 'Javascript',
+    typelanguage: 'Frontend',
+    testing: false
+  },
+  {
+    id: 2,
+    language: 'PHP',
+    typelanguage: 'Backend',
+    testing: false
+  },
+  {
+    id: 3,
+    language: 'Graphql',
+    typelanguage: 'Backend',
+    testing: true
+  },
 ];
 
 const typeDefs = gql`
-    type Dev {
-        id: ID,
-        language: String,
-        typelanguage: String,
-        testing: Boolean
-    }
-    type Query {
-        getDev: [Dev]
-    }
+  type Dev {
+    id: ID,
+    language: String,
+    typelanguage: String,
+    testing: Boolean
+  }
+  type Query {
+    getDev: [Dev]
+  }
+  type Mutation {
+    addDev(
+      language: String,
+      typelanguage: String,
+      testing: Boolean
+    ): Dev
+  }
 `;
 
 const resolvers = {
-    Query: {
-        getDev: () => dev
+  Query: {
+    getDev: () => devs
+  },
+  Mutation: {
+    addDev: (_, {language, typelanguage, testing }) => {
+      const dev = { language, typelanguage, testing };
+      devs.push(dev);
+      return dev;
     }
+  }
 };
 
 
 const server = new ApolloServer({
-    typeDefs, resolvers
+  typeDefs, resolvers
 });
 
 server.listen(process.env.PORT).then(({ url }) => {
-    console.log(`Server Listening on ${url}`);
+  console.log(`Server Listening on ${url}`);
 });
