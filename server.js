c5817c79bd92cca8ebdebe71f8e8f37583fe6549
Douglas Porto <douglasalexandre7@gmail.com>
2018-09-21 20:32:49 +0000

    AddPost

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require("./resolvers");
+
 require("dotenv").config({ path: 'variables.env'})
+const User = require('./models/User');
+const Post = require('./models/Post');
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => { console.log('DB connected')})
-  .catch(err => console.log(err));
+  .catch(err => console.log(err));      
 
-const typeDefs = gql`
-  type Dev {
-    id: ID,
-    language: String,
-    typelanguage: String,
-    testing: Boolean
-  }
-  type Query {
-    getDev: [Dev]
-  }
-`;
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen(process.env.PORT).then(({ url }) => {
