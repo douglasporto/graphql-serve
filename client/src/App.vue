4e3866d8129bf85ad3b6d500d2effc0c6d2dbc16
Douglas Porto <douglasalexandre7@gmil.com>
2018-09-23 21:50:29 -0300

    Starting Vuetify

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,49 @@
 <template>
-  <div>
-    <h1>App</h1>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- Horizontal Navbar -->
+    <v-toolbar fixed color="primary" dark>
+      <!-- App Title -->
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="hidden-xs-only">
+        <router-link to="/" tag="span" style="cursor: pointer">
+          VueShare
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Search Input -->
+      <v-text-field flex prepend-icon="search" placeholder="Search Posts" color="accent" single-line hide-details></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <!-- Horizontal Navbar links -->
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+          <v-icon class="hidden-sm-only" left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <!-- App content -->
+    <main>
+      <v-container class="mt-4">
+        <router-view/>
+      </v-container>
+    </main>
+  </v-app>
 </template>
+<script>
+  export default {
+    name: "App",
+    computed: {
+      horizontalNavItems() {
+        return [
+          { icon: 'chat', title: 'Posts', link: '/posts' },
+          { icon: 'lock_open', title: 'Sign In', link: '/signin' },
+          { icon: 'create', title: 'Sign Up', link: '/signup' },
+        ]
+      }
+    }
+  }
+</script>
